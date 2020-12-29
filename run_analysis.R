07b9b576f00cd6734757fdcc582e59e36e1a1eea
Wayne Mareci <wayne@mareci.com>
2020-12-28 21:07:29 -0500

    added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,20 @@
 #labels
-features <- read.table("./data/features.txt")
+features <- read.table("features.txt")
+
+#activities
+activity_labels <- read.table("activity_labels.txt")
 
 #raw data into data.frame
-x_train <- read.table("./data/x_train.txt")
-y_train <- read.table("./data/y_train.txt")
+x_train <- read.table("x_train.txt")
+y_train <- read.table("y_train.txt")
 
 #raw activity numbers into data.frame
-x_test <- read.table("./data/x_test.txt")
-y_test <- read.table("./data/y_test.txt")
+x_test <- read.table("x_test.txt")
+y_test <- read.table("y_test.txt")
 
 #subjects indexes into data.frame
-subject_test <- read.table("./data/subject_test.txt")
-subject_train <- read.table("./data/subject_train.txt")
+subject_test <- read.table("subject_test.txt")
+subject_train <- read.table("subject_train.txt")
 
 #merge data.frames
 train <- cbind(subject_train,cbind(y_train,x_train))
@@ -21,17 +24,21 @@ test <- cbind(subject_test,cbind(y_test,x_test))
 names(train) <- c("subject","activity",features[,2])
 names(test) <- c("subject","activity",features[,2])
 
-# eliminate all but std and mean measurements
+#eliminate all but std and mean measurements
 test_subset <- test[,c(1,2,grep("(mean|std)\\(\\)",names(test)))]
 train_subset <- train[,c(1,2,grep("(mean|std)\\(\\)",names(train)))]
 
+#combine test and train data 
 final <- rbind(train_subset,test_subset)
 
-activity_labels <- read.table("./data/activity_labels.txt")
-
 library(dplyr)
+
+#replace activity indexes with descriptive labels
 final <- mutate(final,activity=activity_labels[final$activity,2])
 
+#summarize data by averaging by subject and activity
 subject_activity_group <- group_by(final,subject,activity)
 HumanActivityRecognition <- summarize_all(subject_activity_group,mean)
+
+#convert back to txt file
 write.table(HumanActivityRecognition,row.names=F,file="HumanActivityRecognition.txt")
